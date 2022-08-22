757c385171c9f97cc521e882550a69b78768c90c
Rafael Fernandes <98192761+rafael-f@users.noreply.github.com>
2022-08-22 16:30:15 -0300

    fix: test just to reload actions

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -9,6 +9,7 @@ function App() {
     try {
       const response = await axios.get(`${API_URL}/students/random`);
       const student = response.data;
+      console.log(student)
       if(!student){
         alert("Putz! Não há estudantes cadastrados para o sorteio!");
       } else {
